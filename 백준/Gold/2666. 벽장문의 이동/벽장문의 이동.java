d63e3cc7148825d6dba6daf18b11ee2880c2d549
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-09-12 07:27:50 +0900

    [Gold V] Title: 벽장문의 이동, Time: 100 ms, Memory: 11696 KB -BaekjoonHub

---
+++ 백준/Gold/2666. 벽장문의 이동/벽장문의 이동.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static int n, size, a, b;
+	static int result;
+	static int[] arr;
+	static int[] open;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		n = Integer.parseInt(br.readLine()); // 벽장의 개수
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		// 초기에 열려있는 벽장
+		open = new int[2];
+		for (int i = 0; i < 2; i++) {
+			open[i] = Integer.parseInt(st.nextToken());
+		}
+
+		size  = Integer.parseInt(br.readLine()); // 사용할 벽장 길이
+		arr = new int[size];
+ 		for (int i = 0; i < size; i++) {
+			 arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		 result = Integer.MAX_VALUE;
+		 dfs(0, 0);
+
+		System.out.println(result);
+	}
+
+	private static void dfs(int idx, int total) {
+		if (idx == size) {
+			result = Math.min(result, total);
+			return;
+		}
+
+		for (int i = 0; i < 2; i++) {
+			int tmp = open[i];
+			int diff = Math.abs(open[i] - arr[idx]);
+			open[i] = arr[idx];
+			dfs(idx + 1, total + diff);
+			open[i] = tmp;
+		}
+	}
+}
