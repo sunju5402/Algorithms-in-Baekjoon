a9ffcfcafdb5c1a63689947d959f711c0edf112c
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2023-09-12 08:32:33 +0900

    [Gold V] Title: 벽장문의 이동, Time: 76 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Gold/2666. 벽장문의 이동/벽장문의 이동.java
+++ 백준/Gold/2666. 벽장문의 이동/벽장문의 이동.java
@@ -3,10 +3,10 @@ import java.util.*;
 
 public class Main {
 
-	static int n, size, a, b;
+	static int n, size;
 	static int result;
 	static int[] arr;
-	static int[] open;
+	static int[][][] memo;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -14,10 +14,8 @@ public class Main {
 
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		// 초기에 열려있는 벽장
-		open = new int[2];
-		for (int i = 0; i < 2; i++) {
-			open[i] = Integer.parseInt(st.nextToken());
-		}
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
 
 		size  = Integer.parseInt(br.readLine()); // 사용할 벽장 길이
 		arr = new int[size];
@@ -25,26 +23,23 @@ public class Main {
 			 arr[i] = Integer.parseInt(br.readLine());
 		}
 
-		 result = Integer.MAX_VALUE;
-		 dfs(0, 0);
-
+		memo = new int[size][n + 1][n + 1];
+		result = dfs(0, a, b);
 		System.out.println(result);
 	}
 
-	private static void dfs(int idx, int total) {
-        if (result <= total) return;
-        
+	private static int dfs(int idx, int a, int b) {
 		if (idx == size) {
-			result = Math.min(result, total);
-			return;
+			return 0;
 		}
 
-		for (int i = 0; i < 2; i++) {
-			int tmp = open[i];
-			int diff = Math.abs(open[i] - arr[idx]);
-			open[i] = arr[idx];
-			dfs(idx + 1, total + diff);
-			open[i] = tmp;
-		}
+		if (memo[idx][a][b] != 0) return memo[idx][a][b];
+
+		int aDiff = Math.abs(a - arr[idx]) + dfs(idx + 1, arr[idx], b);
+		int bDiff = Math.abs(b - arr[idx]) + dfs(idx + 1, a, arr[idx]);
+		int total = result + Math.min(aDiff, bDiff);
+		memo[idx][a][b] = total;
+		
+		return total;
 	}
 }
