420cbe41a9b6f18d6931cfd4f16beaba5277c6e5
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2024-02-25 22:58:54 +0900

    [Gold V] Title: 누가 이길까, Time: 516 ms, Memory: 44956 KB -BaekjoonHub

--- 백준/Gold/28449. 누가 이길까/누가 이길까.java
+++ 백준/Gold/28449. 누가 이길까/누가 이길까.java
@@ -9,7 +9,6 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 
-
 		int[] hi = new int[N];
 		int[] arc = new int[M];
 
@@ -29,32 +28,17 @@ public class Main {
 		long[] result = new long[3];
 		int idx = 0;
 		int[] sameCnt = new int[Math.max(hi[N - 1], arc[M - 1]) + 1];
-		if (N > M) { // arc팀 참가자가 적을 경우
-			for (int i = 0; i < M; i++) {
-				while (idx < N && hi[idx] <= arc[i]) {
-					if (hi[idx] == arc[i]) {
-						sameCnt[arc[i]]++;
-					}
-					idx++;
+		for (int i = 0; i < M; i++) {
+			while (idx < N && hi[idx] <= arc[i]) {
+				if (hi[idx] == arc[i]) {
+					sameCnt[arc[i]]++;
 				}
-
-				result[0] += (N - idx);
-				result[1] += (idx - sameCnt[arc[i]]);
-				result[2] += sameCnt[arc[i]];
+				idx++;
 			}
-		} else { // hi 팀 참가자가 적을 경우
-			for (int i = 0; i < N; i++) {
-				while (idx < M && hi[i] >= arc[idx]) {
-					if (hi[i] == arc[idx]) {
-						sameCnt[hi[i]]++;
-					}
-					idx++;
-				}
 
-				result[0] += (idx - sameCnt[hi[i]]);
-				result[1] += (M - idx);
-				result[2] += sameCnt[hi[i]];
-			}
+			result[0] += (N - idx);
+			result[1] += (idx - sameCnt[arc[i]]);
+			result[2] += sameCnt[arc[i]];
 		}
 
 		System.out.print(result[0] + " " + result[1] + " " + result[2]);
