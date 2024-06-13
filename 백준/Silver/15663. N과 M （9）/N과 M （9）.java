f9a705bae24535c9951bbfd3e4c8d5fe1492c078
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2024-06-14 00:12:19 +0900

    [Silver II] Title: N과 M (9), Time: 320 ms, Memory: 62828 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N, M;
+	static StringBuilder sb;
+	static int[] arr;
+	static boolean[] visited;
+	static List<Integer> list;
+	static Set<String> set;
+    
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		sb = new StringBuilder();
+		arr = new int[M];
+		visited = new boolean[N];
+		list = new ArrayList<>();
+		set = new HashSet<>();
+		st = new StringTokenizer(br.readLine(), " ");
+
+		for (int i = 0; i < N; i++) {
+			list.add(Integer.parseInt(st.nextToken()));
+		}
+
+		Collections.sort(list);
+
+		permutation(0);
+		System.out.print(sb.toString());
+	}
+
+	private static void permutation(int depth) {
+		if (depth == M) {
+			String str = "";
+			for (int n : arr) {
+				str += n + " ";
+			}
+
+			if (set.contains(str)) {
+				return;
+			}
+
+			sb.append(str).append("\n");
+			set.add(str);
+			return;
+		}
+
+		for (int i = 0; i < N; i++) {
+			if (!visited[i]) {
+				visited[i] = true;
+				arr[depth] = list.get(i);
+				permutation(depth + 1);
+				visited[i] = false;
+			}
+		}
+	}
+}
