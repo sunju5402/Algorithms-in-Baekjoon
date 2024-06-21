6a04000b4595bff1d9735a715fb1db6a97f7efc2
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2024-06-21 20:02:50 +0900

    [Gold V] Title: 문자열 게임 2, Time: 336 ms, Memory: 34024 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.java
@@ -23,21 +23,21 @@ public class Main {
 
 			int max = Integer.MIN_VALUE;
 			int min = Integer.MAX_VALUE;
-			for (int i = 0; i < list.size(); i++) {
-				if (list.get(i).size() < K) {
+			for (ArrayList<Integer> alphabet : list) {
+				if (alphabet.size() < K) {
 					continue;
 				}
 
-				for (int j = 0; j <= list.get(i).size() - K; j++) {
-					min = Math.min(min, list.get(i).get(j + K - 1) - list.get(i).get(j) + 1);
-					max = Math.max(max, list.get(i).get(j + K - 1) - list.get(i).get(j) + 1);
+				for (int j = 0; j <= alphabet.size() - K; j++) {
+					min = Math.min(min, alphabet.get(j + K - 1) - alphabet.get(j) + 1);
+					max = Math.max(max, alphabet.get(j + K - 1) - alphabet.get(j) + 1);
 				}
 			}
 
 			if (min == Integer.MAX_VALUE) {
 				sb.append(-1).append("\n");
 			} else {
-				sb.append(min + " " + max).append("\n");
+				sb.append(min).append(" ").append(max).append("\n");
 			}
 		}
 
