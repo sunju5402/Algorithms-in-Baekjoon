d53817ac283303d6e91e7da30ea5efaf22982c25
sunju5402 <68212526+sunju5402@users.noreply.github.com>
2024-07-21 02:40:55 +0900

    [Gold V] Title: 0 만들기, Time: 240 ms, Memory: 35308 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.java
+++ 백준/Gold/7490. 0 만들기/0 만들기.java
@@ -1,25 +1,17 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-	static List<String> list;
+	static StringBuilder sb;
 	static int N;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int T = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
+		sb = new StringBuilder();
 
 		for (int t = 0; t < T; t++) {
 			N = Integer.parseInt(br.readLine());
-			list = new ArrayList<>();
 			dfs(1, 1, "1");
-
-			Collections.sort(list);
-
-			for (String s : list) {
-				sb.append(s).append("\n");
-			}
 			sb.append("\n");
 		}
 
@@ -61,13 +53,13 @@ public class Main {
 			}
 
 			if (total == 0) {
-				list.add(s);
+				sb.append(s).append("\n");
 			}
 			return;
 		}
 
+		dfs(n + 1, depth + 1, s + " " + (n + 1));
 		dfs(n + 1, depth + 1, s + "+" + (n + 1));
 		dfs(n + 1, depth + 1, s + "-" + (n + 1));
-		dfs(n + 1, depth + 1, s + " " + (n + 1));
 	}
 }
